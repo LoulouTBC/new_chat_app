3695d2fb632150a316c3f80360348feb1f083c47
LoulouTBC <loulouibrahim21@gmail.com>
2023-08-01 19:56:34 +0300

    init project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:chat_app/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,46 @@
+import 'package:chat_app/components/customTextField.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff2B475E),
+      body: Column(
+        children: [
+          Image.asset('assets/images/scholar.png'),
+          Text(
+            'Scholar Chat',
+            style: TextStyle(
+                fontSize: 32, color: Colors.white, fontFamily: 'Pacifico'),
+          ),
+          Text(
+            'LOGIN',
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+            ),
+          ),
+          CustomTextField(
+            hintText: 'Email',
+          ),
+          CustomTextField(
+            hintText: 'Password',
+          ),
+          Container(
+            width: double.infinity,
+            height: 60,
+            child: Text('LOGIN'),
+            decoration:BoxDecoration(
+              color: Colors.white
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
