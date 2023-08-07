c58386ee177ae69307402fab5e86d4805ba4798f
LoulouTBC <loulouibrahim21@gmail.com>
2023-08-07 20:27:37 +0300

    login screen

--- lib/components/customTextField.dart
+++ lib/components/customTextField.dart
@@ -8,14 +8,17 @@ class CustomTextField extends StatelessWidget {
   String? hintText;
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-          hintText: hintText,
-          hintStyle: TextStyle(color: Colors.white),
-          enabledBorder:
-              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-          border:
-              OutlineInputBorder(borderSide: BorderSide(color: Colors.white))),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: TextField(
+        decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: TextStyle(color: Colors.white),
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white))),
+      ),
     );
   }
 }
